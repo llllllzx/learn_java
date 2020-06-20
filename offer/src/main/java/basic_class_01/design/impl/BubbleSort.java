88fd76740b363b6a9d7784e15497f58b58e12740
liuzhaoxin.yaya <liuzhaoxin.yaya@bytedance.com>
2020-06-20 10:40:23 +0800

    20200619

--- offer/src/main/java/basic_class_01/design/impl/BubbleSort.java
+++ offer/src/main/java/basic_class_01/design/impl/BubbleSort.java
@@ -10,13 +10,14 @@ import static basic_class_01.Code_04_QuickSort.swap;
 public class BubbleSort implements Sort {
     @Override
     public void sort(int[] arr) {
-        if (arr == null || arr.length == 0) {
-            return;
+        if(arr == null || arr.length < 2){
+            return ;
         }
-        for (int i = arr.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1])
-                    swap(arr, j, j + 1);
+        for(int e = arr.length -1;e > 0;e--){
+            for(int i = 0; i < e;i++){
+                if(arr[i] > arr[i+1]){
+                    swap(arr,i,i+1);
+                }
             }
         }
     }
--- offer/src/main/java/basic_class_01/design/impl/InsertionSort.java
+++ offer/src/main/java/basic_class_01/design/impl/InsertionSort.java
@@ -11,12 +11,12 @@ import static basic_class_01.Code_01_InsertionSort.swap;
 public class InsertionSort implements Sort {
     @Override
     public void sort(int[] arr) {
-        if (arr == null || arr.length == 0) {
+        if(arr == null || arr.length < 2){
             return;
         }
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i - 1; j >= 0 && arr[j] > arr[j+1]; j--) {
-                    swap(arr,j,j+1);
+        for(int i = 1;i < arr.length;i++){
+            for(int j = i -1;j >= 0 && arr[j] > arr[j+1];j--){
+                swap(arr,j,j+1);
             }
         }
     }
